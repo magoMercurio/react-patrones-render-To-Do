fa81208c09f0c549ed5a28f8d843015aec74993b
mago_d_Mercurio <davila.feria@gmail.com>
2023-09-25 10:04:59 -0500

    Update index.js

--- src/App/index.js
+++ src/App/index.js
@@ -1,14 +1,71 @@
 import React from 'react';
+import { useState } from 'react';
 import { TodoProvider } from '../TodoContext';
 import { AppUI } from './AppUI';
+/* import { TodoList } from '../TodoList';
+ *//* import { TodoHeader } from '../TodoHeader';
+import { TodoCounter } from '../TodoCounter';
+import { TodoSearch } from '../TodoSearch';
+ */
 
 
 function App() {
+  const [state, setstate] = useState('');
+  return (
+    <React.Fragment>
+    <TodoHeader />
+    <TodoList />
+    </React.Fragment>
+  )
+}
+
+function TodoHeader() {
+  return (
+    <React.Fragment>
+      <TodoCounter />
+      <TodoSearch />
+    </React.Fragment>
+  )
+}
+
+function TodoList() {
+  return (
+    <>
+    <TodoItem />
+    </>
+  )
+}
+
+function TodoCounter() {
+  return (
+    <>
+    <p>TodoCounter</p>
+    </>
+  )
+}
+
+function TodoSearch() {
+  return (
+    <>
+    <p>TodoSearch</p>
+    </>
+  )
+}
+
+function TodoItem() {
+  return (
+    <>
+    <p>TodoItem</p>
+    </>
+  )
+}
+
+/* function App() {
   return (
     <TodoProvider>
       <AppUI />
     </TodoProvider>
   );
-}
+} */
 
 export default App;
