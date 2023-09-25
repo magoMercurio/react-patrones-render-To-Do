d2ae50809b0988141f8ddb097a2038b617dd1a24
mago_d_Mercurio <davila.feria@gmail.com>
2023-09-25 12:02:45 -0500

    Update index.js

--- src/App/index.js
+++ src/App/index.js
@@ -3,74 +3,19 @@ import { useState } from 'react';
 import { TodoProvider } from '../TodoContext';
 import { AppUI } from './AppUI';
 /* import { TodoList } from '../TodoList';
- *//* import { TodoHeader } from '../TodoHeader';
+import { TodoHeader } from '../TodoHeader';
 import { TodoCounter } from '../TodoCounter';
-import { TodoSearch } from '../TodoSearch';
- */
+import { TodoSearch } from '../TodoSearch'; */
+
+
 
 
 function App() {
-  const [state, setstate] = useState('estado compartido');
-  return (
-    <React.Fragment>
-    <TodoHeader>
-      <TodoCounter />
-      <TodoSearch />
-    </TodoHeader>
-
-    <TodoList>
-      <TodoItem state={state} />
-    </TodoList>
-    </React.Fragment>
-  )
-}
-
-function TodoHeader({ children }) {
-  return (
-    <header>
-      {children}
-    </header>
-  )
-}
-
-function TodoList({ children }) {
-  return (
-    <section className='TodoList-container'>
-      {children}
-    </section>
-  )
-}
-
-function TodoCounter() {
-  return (
-    <>
-    <p>TodoCounter</p>
-    </>
-  )
-}
-
-function TodoSearch() {
-  return (
-    <>
-    <p>TodoSearch</p>
-    </>
-  )
-}
-
-function TodoItem({ state }) {
-  return (
-    <>
-    <p>TodoItem: {state}</p>
-    </>
-  )
-}
-
-/* function App() {
   return (
     <TodoProvider>
       <AppUI />
     </TodoProvider>
   );
-} */
+}
 
 export default App;
