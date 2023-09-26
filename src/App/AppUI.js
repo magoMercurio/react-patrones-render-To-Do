286cfdc1b1be462f05589cb487e7e228612b3b87
mago_d_Mercurio <davila.feria@gmail.com>
2023-09-26 10:49:13 -0500

    create TodoHeader, move context to AppUI

--- src/App/AppUI.js
+++ src/App/AppUI.js
@@ -10,6 +10,7 @@ import { EmptyTodos } from '../EmptyTodos';
 import { TodoForm } from '../TodoForm';
 import { CreateTodoButton } from '../CreateTodoButton';
 import { Modal } from '../Modal';
+import TodoHeader from '../TodoHeader';
 
 function AppUI() {
   const {
@@ -20,12 +21,25 @@ function AppUI() {
     deleteTodo,
     openModal,
     setOpenModal,
+    totalTodos,
+    completedTodos,
+    searchValue,
+    setSearchValue,
   } = React.useContext(TodoContext);
   
   return (
     <React.Fragment>
-      <TodoCounter />
-      <TodoSearch />
+
+      <TodoHeader>
+        <TodoCounter 
+          totalTodos={totalTodos}
+          completedTodos={completedTodos}
+        />
+        <TodoSearch
+          searchValue={searchValue}
+          setSearchValue={setSearchValue}
+          />
+      </TodoHeader>
 
       <TodoList>
         {error && <TodosError />}
--- src/App/index.js
+++ src/App/index.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { useState } from 'react';
+/* import { useState } from 'react'; */
 import { TodoProvider } from '../TodoContext';
 import { AppUI } from './AppUI';
 /* import { TodoList } from '../TodoList';
